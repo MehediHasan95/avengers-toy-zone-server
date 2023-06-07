3624661c4d5e465e21e1b84221fa323cdbfbefb2
MehediHasan95 <imehedihasan95@gmail.com>
2023-06-07 22:08:45 +0600

    mongodb connection success

--- index.js
+++ index.js
@@ -3,13 +3,28 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require("mongodb");
 
 // middleware
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(
+  `mongodb+srv://${process.env.BUCKET_NAME}:${process.env.SECRET_KEY}@cluster0.hfakwfc.mongodb.net/?retryWrites=true&w=majority`,
+  {
+    serverApi: {
+      version: ServerApiVersion.v1,
+      strict: true,
+      deprecationErrors: true,
+    },
+  }
+);
+
 async function run() {
   try {
+    await client.connect();
+    await client.db("admin").command({ ping: 1 });
+    console.log("You successfully connected to MongoDB!");
     app.get("/", (req, res) => res.send("AvengersToyZone Server is Running"));
   } finally {
     app.listen(port, () =>
