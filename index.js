73a0a86b56c8290d1eba88c545f4186518c4e4a7
MehediHasan95 <imehedihasan95@gmail.com>
2023-06-08 19:59:47 +0600

    toys delete method complete

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5000;
-const { MongoClient, ServerApiVersion } = require("mongodb");
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 
 // middleware
 app.use(cors());
@@ -35,6 +35,29 @@ async function run() {
     app.get("/alltoys", async (req, res) => {
       const results = await toyCollection.find({}).toArray();
       res.send(results);
+    });
+
+    // GET SPECIFIC DATA
+    app.get("/toy/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.findOne(query);
+      res.send(result);
+    });
+
+    // GET MY TOYS
+    app.get("/mytoys", async (req, res) => {
+      const results = await toyCollection
+        .find({ uid: { $eq: req?.query?.uid } })
+        .toArray();
+      res.send(results);
+    });
+
+    app.delete("/mytoys/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await toyCollection.deleteOne(query);
+      res.send(result);
     });
 
     // POST METHOD
